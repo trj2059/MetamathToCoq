7c8b8fe2f706a20bc6f7f78f2165e54dc6aa47c9
Tony Johnson <trj2059@gmail.com>
2022-04-12 20:53:08 -0500

    Started softwarefoundations tutorial

--- CoqMetamath.v
+++ CoqMetamath.v
@@ -1497,3 +1497,23 @@ Proof.
     end
   ).
 Qed.
+
+Inductive day : Type :=
+  | monday
+  | tuesday
+  | wednesday
+  | thursday
+  | friday
+  | saturday
+  | sunday.
+
+Definition next_weekday (d:day) : day :=
+  match d with
+  | monday => tuesday
+  | tuesday => wednesday
+  | wednesday => thursday
+  | thursday => friday
+  | friday => monday
+  | saturday => monday
+  | sunday => monday
+  end.
